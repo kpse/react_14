0bb45bd9199e13b1b11f5fdeabe6e1a65e39e702
Suo Qin <kpsesuo@gmail.com>
2016-02-21 00:20:04 +0800

    add state to form

--- src/components/lessons/components/CommentFormComponent.js
+++ src/components/lessons/components/CommentFormComponent.js
@@ -5,10 +5,26 @@ import React from 'react';
 require('styles/lessons/components/CommentForm.less');
 
 class CommentFormComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {author: '', text: ''};
+  }
+
+  authorChange(e) {
+    console.log(e.target.value);
+    console.log(this.state.author);
+    this.setState({author: e.target.value});
+  }
+
+  textChange(e) {
+    console.log(this.state.text);
+    this.setState({text: e.target.value});
+  }
+
   render() {
     return (<form className="comment-form">
-      <input type="text" placeholder="your name"/>
-      <input type="text" placeholder="say something"/>
+      <input type="text" placeholder="your name" value={this.state.author} onChange={(e) => this.authorChange(e)}/>
+      <input type="text" placeholder="say something" value={this.state.text} onChange={(e) => this.textChange(e)}/>
       <input type="submit" value="Post"/>
     </form>)
   }
